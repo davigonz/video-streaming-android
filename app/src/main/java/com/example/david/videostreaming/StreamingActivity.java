2bc9b3f820b5b8bc397a7652b3217e23d2cda1ec
David González <dgonzalez@solidgear.es>
2017-04-01 20:44:17 +0200

    First commit

---
+++ app/src/main/java/com/example/david/videostreaming/StreamingActivity.java
@@ -0,0 +1,85 @@
+package com.example.david.videostreaming;
+
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.SurfaceView;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+public class StreamingActivity extends AppCompatActivity {
+
+    private SimpleExoPlayer player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_streaming);
+
+        // Create a default TrackSelector
+        Handler mainHandler = new Handler();
+
+        // Estimates bandwidth
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+
+        // TrackSelector is used When a piece of media contains multiple tracks of a given type,
+        // for example multiple video tracks in different qualities or multiple audio tracks in
+        // different languages
+        // Adaptative: selected track is updated to be the one of highest quality given the
+        // current network conditions and the state of the buffer
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        // Controls buffering of media
+        LoadControl loadControl = new DefaultLoadControl();
+
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
+
+        SimpleExoPlayerView simpleExoPlayerView = (SimpleExoPlayerView) findViewById(R.id.video_player);
+
+        // Bind the player to the view.
+        simpleExoPlayerView.setPlayer(player);
+
+        // Create DataSource instance through which media data is loaded.
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, String.valueOf(R.string.app_name)));
+
+
+        // Produces Extractor instances for parsing the media data.
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+        String mp4VideoUrl = "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4";
+
+        Uri mp4VideoUri = Uri.parse(mp4VideoUrl);
+
+        // This is the MediaSource representing the media to be played.
+        MediaSource videoSource = new ExtractorMediaSource(mp4VideoUri,
+                dataSourceFactory, extractorsFactory, null, null);
+
+        // Prepare the player with the source.
+        player.prepare(videoSource);
+
+        player.setPlayWhenReady(true);
+    }
+}
